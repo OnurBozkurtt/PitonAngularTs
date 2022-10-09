5d51f56d2aeda593f67ea3ec847b24e34cf30dff
Onur Bozkurt <onurbozkurt166@gmail.com>
2022-10-09 22:09:02 +0300

    Product

--- src/app/_ProductDetails/productDetails.component.ts
+++ src/app/_ProductDetails/productDetails.component.ts
@@ -3,23 +3,24 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../_services/product.service';
 import { Product } from '../_Models/product';
 
-
-
 @Component({
   selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  templateUrl: './productdetails.component.html',
+  styleUrls: ['./productdetails.component.css'],
 })
+export class ProductDetailsComponent implements OnInit {
+  product!: Product;
 
-    export class ProductDetailsComponent implements OnInit {
-        product!: Product;
-   
-    constructor( private route: ActivatedRoute, private productservice: ProductService ) { }
+  constructor(
+    private route: ActivatedRoute,
+    private productservice: ProductService
+  ) {}
 
-    ngOnInit() {
-        let id =+ this.route.snapshot.params['id'];
+  ngOnInit() {
+    let id = +this.route.snapshot.params['id'];
 
-        this.productservice.GetProductbyID(id).subscribe(result => this.product = result);
+    this.productservice
+      .GetProductbyID(id)
+      .subscribe((result) => (this.product = result));
   }
-
 }
